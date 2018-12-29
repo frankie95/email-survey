cc015dd91a03e2ad7fbe8f48a478b7752f18102f
Frankie Chau <frankiechau20044e@yahoo.com.hk>
2018-12-30 01:07:08 +0800

    30-12-18

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import{Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux'
 import App from './components/App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store=createStore(()=>[],{},applyMiddleware())
+
+
+ReactDOM.render(
+
+
+
+<Provider store={store}><App /></Provider>
+    
+    
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
