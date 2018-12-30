9b647ba64043035ea7281e5fcda9ba4f718dd8c5
Frankie Chau <frankiechau20044e@yahoo.com.hk>
2018-12-31 00:33:16 +0800

    31-12-18

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const Header =()=><h2>Header</h2>
+const Dashboard =()=><h2>Dashboard</h2>
+const SurveyNew =()=><h2>SurveyNew</h2>
+const Landing =()=><h2>Landing</h2>
 
 const App=()=>{
 
 return(
 
-<div>Hi there!</div>
+<div>
+<BrowserRouter>
+<div>
+<Header/>
+<Route path="/" component={Landing} exact></Route>
+<Route path="/surveys" component={Dashboard} exact></Route>
+<Route path="/surveys/new" component={SurveyNew} exact></Route>
+<Route path="/" component={Landing}></Route>
+
+</div>
+</BrowserRouter>
+</div>
 
 )
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom';
 import{Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux'
 import App from './components/App';
+import reducers from './reducers'
+
+const store=createStore(reducers,{},applyMiddleware())
 
 
-const store=createStore(()=>[],{},applyMiddleware())
-
-
-ReactDOM.render(
-
-
-
-<Provider store={store}><App /></Provider>
-    
-    
-    , document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
