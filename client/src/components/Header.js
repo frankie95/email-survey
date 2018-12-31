dea1312ccd404ce3ea4bdd5dd49a8d0cf1669e7f
Frankie Chau <frankiechau20044e@yahoo.com.hk>
2019-01-01 02:44:11 +0800

    1-1-19

---
+++ client/src/components/Header.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react' 
+
+class Header extends Component{
+render(){
+
+return(
+
+<nav>
+  <div className="nav-wrapper">
+    <a href="" className="left brand-logo">Email Survey</a>
+    <ul className="right">
+      <li><a href="">Login With Google</a></li>
+    </ul>
+  </div>
+</nav>
+
+)
+
+
+
+}
+
+
+
+
+}
+
+export default Header
