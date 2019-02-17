0246fe319393c34c89bd943e279a6054b8352a2e
Frankie Chau <frankiechau20044e@yahoo.com.hk>
2019-02-18 00:23:37 +0800

    18-2-19

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <div class='fixed-action-btn'>
+        <a class='btn-floating btn-large red'>
+          <i class='material-icons'>add</i>
+        </a>
+      </div>
+    </div>
+  )
+}
+export default Dashboard
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,7 +5,13 @@ const Mailer = require('../services/Mailer')
 const Survey = mongoose.model('surveys')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks',(req, res) => {
+    res.send('Thanks for voting!')
+
+
+  })
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body
     console.log(req.body)
     const survey = new Survey({
@@ -19,6 +25,14 @@ module.exports = app => {
       dateSent: Date.now()
     })
     const mailer = new Mailer(survey, surveyTemplate(survey))
-    mailer.send()
+    try {
+      await mailer.send()
+      await survey.save()
+      req.user.credits -= 1
+      const user = await req.user.save()
+      res.send(user)
+    } catch (err) {
+      res.status(422).send(err)
+    }
   })
 }
